4931446c8970dd0714213877380d0b704a7a6bcc
Biniyam Girma <biniswengineer@gmail.com>
2025-08-20 10:28:22 +0300

    update

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,8 +1,7 @@
-CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 CREATE TABLE Books (
-	book_id INT PRIMARY KEY,
+        book_id INT PRIMARY KEY,
     title VARCHAR(130),
     FOREIGN KEY (author_id) REFERENCES Authors(author_id),
     price DOUBLE,
@@ -10,26 +9,29 @@ CREATE TABLE Books (
 );
 
 CREATE TABLE Authors (
-	author_id INT PRIMARY KEY,
+        author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
 CREATE TABLE Customers (
-	customer_id INT PRIMARY KEY,
+        customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
 CREATE TABLE Orders (
-	order_id INT PRIMARY KEY,
+        order_id INT PRIMARY KEY,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
 
 CREATE TABLE Order_Details (
-	orderdetailid INT PRIMARY KEY,
-	FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+        orderdetailid INT PRIMARY KEY,
+        FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 );
+
+
+
